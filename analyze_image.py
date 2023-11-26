84d8d0c9d3c323d38c59807f32f93ec70a374f3c
Johann-Peter Hartmann <johann-peter.hartmann@mayflower.de>
2023-11-26 14:39:49 +0100

    Use hustenbonbons branch unless the official one is updated

--- analyze_image.py
+++ analyze_image.py
@@ -2,12 +2,37 @@ import base64
 import requests
 from io import BytesIO
 from PIL import Image
+from langchain.prompts import PromptTemplate
 import os
 
 # OpenAI API Key
 api_key = os.environ['OPENAI_API_KEY']
 
-def analyze_image(image: Image):
+
+ROBOT_PROMPT = """You are a robot dog, and this is what your eyes see right now. 
+It is not an image or a picture. """
+
+EXPLORE_PROMPT = """Describe what you can see, in as much detail as possible and with the direction you would need to walk to get there. 
+Start your answer with with 'I see ' """
+
+DIRECTION_PROMPT = """ This is what you can see right now. There is a {item}. Please return the direction you would need to walk to get there, and the Distance. Answer with a JSON object like this: {{"direction": "left", "distance": "10 cm"}}"""
+
+def explore_image(image: Image) -> str:
+  """You can see what is around you."""
+  return analyze_image(image, EXPLORE_PROMPT)
+
+def get_direction_from_image(image: Image, item: str) -> str:
+  """Get the direction to reach an item"""
+  prompt_template = PromptTemplate.from_template(DIRECTION_PROMPT)
+  prompt = prompt_template.render(item=item)
+  return analyze_image(image, prompt)
+
+
+
+
+def analyze_image(image: Image, prompt: str)-> str:
+  """Analyses an image and returns a description"""
+  full_prompt = ROBOT_PROMPT + prompt
   buffered = BytesIO()
   image.save(buffered, format="JPEG")
   base64_image = base64.b64encode(buffered.getvalue()).decode("utf-8")
@@ -25,7 +50,7 @@ def analyze_image(image: Image):
         "content": [
           {
             "type": "text",
-            "text": "What’s in this image?"
+            "text": full_prompt,
           },
           {
             "type": "image_url",
